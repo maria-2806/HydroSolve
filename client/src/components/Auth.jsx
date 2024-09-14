1a3def38f961e76ef89ef3610add3d0103723dd7
Maria <mariasuhail762@gmail.com>
2024-09-14 21:15:16 +0530

    Change fetch

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -23,7 +23,7 @@ const Auth = () => {
     const endpoint = isSignUp ? '/signup' : '/login';
 
     try {
-      const response = await axios.post(`http://localhost:5000${endpoint}`, {
+      const response = await axios.post(`https://hydrosolve.onrender.com${endpoint}`, {
         name: isSignUp ? name : undefined,
         email,
         password,
